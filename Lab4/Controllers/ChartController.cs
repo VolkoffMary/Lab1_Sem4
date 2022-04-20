59307a985d70b9d538cfa3fc7e6c22716d548327
VolkoffMary <volkoffmary@gmail.com>
2022-04-21 00:06:00 +0300

    Step 1.5

---
+++ Lab4/Controllers/ChartController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab4.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChartController : ControllerBase
+    {
+        private readonly DBUniWorkersContext _context;
+        public ChartController(DBUniWorkersContext context)
+        { 
+            _context = context; 
+        }
+
+        [HttpGet("JsonDataPeopleByDepartment")]
+        public JsonResult JsonDataPeopleByDepartment()
+        {
+            var departments = _context.Departments.ToList();
+            List<object> peopleByDepartment = new List<object>();
+            peopleByDepartment.Add(new[] { "Кафедра", "Кількість працівників" });
+            foreach (var d in departments)
+            {
+                d.People = _context.People.Where(p => p.DepartmentId == d.Id).ToList();
+                peopleByDepartment.Add(new object[] { d.DepartmentName, d.People.Count() });
+            }
+            return new JsonResult(peopleByDepartment);
+        }
+
+        [HttpGet("JsonDataSalaryPerPosition")]
+        public JsonResult JsonDataSalaryPerPosition()
+        {
+            var positions = _context.Positions.ToList();
+            List<object> salaryPerPosition = new List<object>();
+            salaryPerPosition.Add(new[] { "Посада", "Зарплата" });
+            foreach (var p in positions)
+            {
+                salaryPerPosition.Add(new object[] { p.PositionName, p.Salary });
+            }
+            return new JsonResult(salaryPerPosition);
+        }
+    }
+}
